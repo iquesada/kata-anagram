ddacb292748450246800275339e6fdd35a78e43b
Iñaki Quesada <iquesada@piedresybarro.com>
2019-11-27 20:40:48 +0100

    Generating descendants

--- lib/generator.ex
+++ lib/generator.ex
@@ -6,8 +6,23 @@ defmodule Generator do
     |> Enum.map(&Enum.join/1)
   end
 
+  def descendants(word) do
+    word
+    |> String.graphemes()
+    |> combinations(String.length(word) - 1)
+    |> Enum.map(&Enum.join/1)
+  end
+
   defp permutations([]), do: [[]]
 
   defp permutations(list),
     do: for(elem <- list, rest <- permutations(list -- [elem]), do: [elem | rest])
+
+  def combinations(list, num)
+  def combinations(_list, 0), do: [[]]
+  def combinations(list = [], _num), do: list
+
+  def combinations([head | tail], num) do
+    Enum.map(combinations(tail, num - 1), &[head | &1]) ++ combinations(tail, num)
+  end
 end
--- test/generator_test.exs
+++ test/generator_test.exs
@@ -9,4 +9,13 @@ defmodule GeneratorTest do
       assert Enum.sort(Generator.siblings(word)) == expected_result
     end
   end
+
+  describe "descendants/1" do
+    test "generate all string removing just one letter" do
+      word = "kata"
+      expected_result = Enum.sort(~w(ata kta kaa kat))
+
+      assert Enum.sort(Generator.descendants(word)) == expected_result
+    end
+  end
 end
